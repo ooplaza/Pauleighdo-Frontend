a55343858eeefb2f7571540ca2bdb9a3a53d7b4b
orlyplaza <orly.plaza@valens-research.com>
2023-11-26 22:19:23 +0800

    Added Tailwind Class

--- app.vue
+++ app.vue
@@ -1,10 +1,14 @@
 <template>
-  <section>
+  <section
+    class="flex flex-col min-h-screen bg-white dark:bg-gray-900 scroll-smooth"
+  >
     <!-- Navigation Bar -->
     <NavigationBar />
     <main>
       <!-- Home Page -->
       <NuxtPage />
     </main>
+    <!-- Footer -->
+    <Footer />
   </section>
 </template>
