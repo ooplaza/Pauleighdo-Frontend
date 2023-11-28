ed6e8e2396b4098e826eda63936caed5c10ab28e
orlyplaza <orly.plaza@valens-research.com>
2023-11-28 12:37:52 +0800

    Changed Footer name to Pauleido

--- components/Footer.vue
+++ components/Footer.vue
@@ -22,7 +22,7 @@
                     </ul>
                 </div>
                 <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span class="block text-sm text-[#606C38] sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Pauleighdo™</a>. All Rights Reserved.</span>
+                <span class="block text-sm text-[#606C38] sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Pauleido™</a>. All Rights Reserved.</span>
             </div>
         </footer>
     </div>
